28042fa1129ccafba9c1f89a853e4721d434a104
Your Name~ <you@example.com>
2022-09-25 12:57:21 +0300

    Updates

---
+++ precache-manifest.65522a793f8db480c03a5225baec1d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9589431a93450147fd01b809b5bffd",
+    "url": "/react-reviews/index.html"
+  },
+  {
+    "revision": "456012a146dd4f093f0c",
+    "url": "/react-reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/react-reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456012a146dd4f093f0c",
+    "url": "/react-reviews/static/js/main.7c5cc42b.chunk.js"
+  },
+  {
+    "revision": "426fc16f2db2543aea9e",
+    "url": "/react-reviews/static/js/runtime-main.79a1a57c.js"
+  }
+]);
